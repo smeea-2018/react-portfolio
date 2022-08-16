d30f4f1c601d6ef0f1de25365845ab8b4d0f6ad3
smeea-2018 <smeeaarshad@gmail.com>
2022-08-16 14:47:12 +0100

    code modification for add reaction

--- src/App.js
+++ src/App.js
@@ -1,39 +1,47 @@
-import logo from "./logo.svg";
-import "./App.css";
-import Nav from "./components/Header";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./container/Home";
+// const name = "Smeea";
+// const navLink = [
+//   {
+//     label: "About Me",
+//     href: "#",
+//   },
+//   { label: "portfolio", href: "#" },
+//   { label: "contact", href: "#" },
+// ];
 
-const name = "Smeea";
-const navLink = [
-  {
-    label: "About Me",
-    href: "#",
-  },
-  { label: "portfolio", href: "#" },
-  { label: "contact", href: "#" },
-];
+// // export const App = () => {
+// //   console.log(name);
+// //   return (
+// //     <div>
+// //       <Nav type={name} />
+// //     </div>
+// //     // <div className="App">
+// //     //   <header className="App-header">
+// //     //     <img src={logo} className="App-logo" alt="logo" />
+// //     //     <p>
+// //     //       Edit <code>src/App.js</code> and save to reload.
+// //     //     </p>
+// //     //     <a
+// //     //       className="App-link"
+// //     //       href="https://reactjs.org"
+// //     //       target="_blank"
+// //     //       rel="noopener noreferrer"
+// //     //     >
+// //     //       Learn React
+// //     //     </a>
+// //     //   </header>
+// //     // </div>
+// //   );
+// // };
 
 function App() {
-  console.log(name);
   return (
-    <div>
-      <Nav type={name} />
-    </div>
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
