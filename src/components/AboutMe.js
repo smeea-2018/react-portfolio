3787950a3d5a9c2549f16da40e76f094df2a7b16
smeea-2018 <smeeaarshad@gmail.com>
2022-08-25 05:10:51 +0100

    form added to contact me page

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,21 +1,40 @@
 import Avatar from "@mui/material/Avatar";
 import Stack from "@mui/material/Stack";
+import Paper from "@mui/material/Paper";
+import Box from "@mui/material/Box";
 export const AboutMe = () => {
   return (
     <div>
-      <h1>About Me</h1>
-      {/* <div sx={{ display: "flex" }}> */}
-      <Stack spacing={2}>
-        <item>
-          <Avatar
-            alt="profile pic"
-            src="../images/weatherdashboard.JPG"
-            sx={{ width: 50, height: 50 }}
-          />
-        </item>
-        <item></item>
-      </Stack>
+      <Paper
+        sx={{
+          "& .MuiTextField-root": { m: 1, width: "40ch" },
+          display: "flex",
+          flexDirection: "column",
+          flexWrap: "wrap",
+          justifyContent: "space-around",
+          alignItems: "center",
+          p: 3,
+          m: 8,
+        }}
+      >
+        <h1>About Me</h1>
+
+        <Stack spacing={2}>
+          <item>
+            <Avatar
+              alt="profile pic"
+              src="../images/weatherdashboard.JPG"
+              sx={{ width: 100, height: 100, alignItems: "center" }}
+            />
+          </item>
+          <item>
+            Full stack web developer, attended Birmingham University boot camp,
+            developed new skills in javaScript, CSS,React ,MySQL/MongoDB.
+            Focused on developing mobile-first web applications involving front
+            and backend.
+          </item>
+        </Stack>
+      </Paper>
     </div>
-    // </div>
   );
 };
--- src/components/ContactMe.js
+++ src/components/ContactMe.js
@@ -4,43 +4,68 @@ import FormHelperText from "@mui/material/FormHelperText";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import { flexbox } from "@mui/system";
+import Typography from "@mui/material/Typography";
+import Paper from "@mui/material/Paper";
+import Divider from "@mui/material/Divider";
 import "../App.css";
+import Stack from "@mui/material/Stack";
 
 export default function Message() {
   return (
-    <div className="flex-class">
-      <div className="flex-item">
-        <h1>ContactMe</h1>
-        {/* <FormControl sx={{ width: "25ch" }}> */}
-      </div>
-      <div>
-        <Box
+    <div>
+      <Paper
+        sx={{
+          "& .MuiTextField-root": { m: 1, width: "40ch" },
+          display: "flex",
+          flexDirection: "column",
+          flexWrap: "wrap",
+          justifyContent: "space-between",
+          alignItems: "center",
+          p: 3,
+          m: 8,
+        }}
+      >
+        <Typography component="h1" variant="h4" align="center">
+          Contact Me
+        </Typography>
+        <Divider />
+        <stack
           component="form"
           sx={{
-            "& .MuiTextField-root": { m: 1, width: "25ch" },
+            p: 5,
+            m: 8,
+            display: "flex",
+            flexDirection: "column",
+            flexWrap: "wrap",
+            justifyContent: "space-between",
           }}
-          noValidate
-          autoComplete="off"
+
+          // onSubmit={handleSubmit(onSubmit)}
         >
-          <div>
-            <TextField required id="outlined-required" label="Name" />
-          </div>
-          <div class="form-control">
-            <input
-              id="my-input"
-              placeholder="Email address"
-              aria-describedby="my-helper-text"
+          <Stack spacing={2}>
+            <TextField
+              required
+              label="Name"
+              variant="outlined"
+              helperText="Please enter your first name."
             />
-          </div>
-          <TextField
-            id="outlined-textarea"
-            label="Message"
-            placeholder="Placeholder"
-            multiline
-          />
-        </Box>
-        {/* </FormControl> */}
-      </div>
+
+            <TextField
+              required
+              label="Email"
+              variant="outlined"
+              helperText="Please enter your email address."
+            />
+
+            <TextField
+              required
+              label="Message"
+              variant="outlined"
+              helperText="Please enter message"
+            />
+          </Stack>
+        </stack>
+      </Paper>
     </div>
   );
 }
