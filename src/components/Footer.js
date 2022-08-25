cab38b563e88442f2937585bdd93c6f6c3a8d2b1
smeea-2018 <smeeaarshad@gmail.com>
2022-08-25 02:14:32 +0100

    Added footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,12 +9,16 @@ import LocationOnIcon from "@mui/icons-material/LocationOn";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import TwitterIcon from "@mui/icons-material/Twitter";
+import Paper from "@mui/material/Paper";
 
 export default function Footer() {
   const [value, setValue] = useState(0);
 
   return (
-    <Box sx={{ width: 500, display: "flex" }}>
+    <Paper
+      sx={{ position: "fixed", bottom: 0, left: 0, right: 0 }}
+      elevation={3}
+    >
       <BottomNavigation
         showLabels
         value={value}
@@ -34,6 +38,36 @@ export default function Footer() {
         />
         <BottomNavigationAction label="twitter" icon={<TwitterIcon />} />
       </BottomNavigation>
-    </Box>
+    </Paper>
   );
 }
+
+// export default function Footer() {
+//   const [value, setValue] = useState(0);
+//   <Box sx={{ pb: 7 }}>
+//     <Paper
+//       sx={{ position: "fixed", bottom: 0, left: 0, right: 0 }}
+//       elevation={3}
+//     >
+//       <BottomNavigation
+//         showLabels
+//         value={value}
+//         onChange={(event, newValue) => {
+//           setValue(newValue);
+//         }}
+//       >
+//         <BottomNavigationAction
+//           label="LinkedIn"
+//           href="https://www.linkedin.com/in/smeea-arshad-9a2986246/"
+//           icon={<LinkedInIcon />}
+//         />
+//         <BottomNavigationAction
+//           label="Github"
+//           href="https://github.com/smeea-2018"
+//           icon={<GitHubIcon />}
+//         />
+//         <BottomNavigationAction label="twitter" icon={<TwitterIcon />} />
+//       </BottomNavigation>
+//     </Paper>
+//   </Box>;
+// }
